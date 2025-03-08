aca70cc3a6246828197f010172bab61c7a83e44a
DoHuy0504 <huyhocbai1906@gmail.com>
2025-03-08 16:20:25 +0700

    add coin and score

---
+++ Assets/Script/GameManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using TMPro;
+public class GameManager : MonoBehaviour
+{
+    private int _score = 0;
+    [SerializeField] TextMeshProUGUI _scoreText;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        updateScore();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void AddScore(int score)
+    {
+        _score += score;
+        updateScore(); // mỗi khi tính điểm xog thì sẽ gọi phương thức này
+    }
+    private void updateScore()
+    {
+        _scoreText.text = _score.ToString();
+    }
+}
